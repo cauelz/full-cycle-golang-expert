b184a14399ab25425f7a3a6d551dd6a50a48c42f
Cauê Zaratin <caue.lasagno.zaratin@gmail.com>
2025-03-16 12:54:43 -0300

    constantes e tipos customizados.

--- chapter1/main.go
+++ chapter1/main.go
@@ -12,9 +12,15 @@ var (
 	preco float64
 )
 
+const constante = "Valor constante"
+
+type Idade int
+
 func main() {
 	valoresPadrao()
 	shortHand()
+	constants()
+	newAge()
 }
 
 func valoresPadrao() {
@@ -32,5 +38,13 @@ func shortHand() {
 	println(nome)
 	println(idade)
 	println(ativo)
+}
 
+func constants() {
+	println("Constantes", constante)
+}
+
+func newAge() {
+	var novaIdade Idade = 30
+	println("Nova idade", novaIdade)
 }
