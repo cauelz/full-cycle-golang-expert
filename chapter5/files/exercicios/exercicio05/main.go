cfe37b69d0c8dfe0a9ab530f80fae566fef70711
Cauê Zaratin <caue.lasagno.zaratin@gmail.com>
2025-06-23 17:50:25 -0300

    feito exercicio 5

--- chapter5/files/exercicios/exercicio05/main.go
+++ chapter5/files/exercicios/exercicio05/main.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
 func main() {
-	
+	// Abre o arquivo de origem para leitura
+	file, err := os.Open("mensagem.txt")
+	if err != nil {
+		fmt.Println("Erro ao abrir arquivo: ", err)
+		return
+	}
+	defer file.Close() // Garante o fechamento do arquivo de origem
+
+	// Cria o arquivo de destino para escrita
+	newFile, err := os.Create("copia.txt")
+	if err != nil {
+		fmt.Println("Erro ao criar arquivo: ", err)
+		return
+	}
+	defer newFile.Close() // Garante o fechamento do arquivo de destino
+
+	// Lê todo o conteúdo do arquivo de origem
+	byteSlice, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Println("Erro ao ler arquivo: ", err)
+		return
+	}
+
+	// Escreve o conteúdo lido no novo arquivo
+	_, err = newFile.Write(byteSlice)
+	if err != nil {
+		fmt.Println("Erro ao escrever arquivo: ", err)
+		return
+	}
+
+	fmt.Println("Arquivo copiado com sucesso!")
 }
